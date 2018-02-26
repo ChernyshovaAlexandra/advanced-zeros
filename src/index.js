57c62ecc9365375d4e755c821917fad481bc69d3
ChernyshovaAlexandra <alexandrachrystal@gmail.com>
2018-02-26 16:42:26 +0300

    Update index.js
    function implemented (not tested))

--- src/index.js
+++ src/index.js
@@ -1,38 +1,42 @@
-function getZerosCount(number, base) {
-    var primDel = [[]];
-    var delCounter = [];
-    var n = base;
-    var curNumber;
-    var i = 2;
-    do {
-        if (n % i === 0) {
-            if (primDel[primDel.length - 1].indexOf(i) === -1 && primDel[primDel.length - 1].length) {
-                primDel.push([]);
-            }
-            primDel[primDel.length - 1].push(i);
-            n /= i;
-        } else {
-            i++
-        }
-    } while (n !== 1)
-
+module.exports = function getZerosCount(number, base) {
+  var primDel = [[]];
+  var delCounter = [];
+  var lengthCounter = [];
+  var n = base;
+  var curNumber;
+  var i = 2;
+  do {
+    if (n % i === 0) {
+      if (primDel[primDel.length - 1].indexOf(i) === -1 && primDel[primDel.length - 1].length) {
+        primDel.push([]);
+      }
+      primDel[primDel.length - 1].push(i);
+      n /= i;
+    } else {
+      i++
+    }
+  } while (n !== 1)
     for (var k = 0; k < primDel.length; k++) {
-        delCounter.push([]);
-        for (var j = 0; j <= number; j++) {
-            curNumber = j;
-
-
-            console.log(curNumber, primDel[k][0])
-            do {
-                if (curNumber % primDel[k][0] === 0) {
-                    console.log('cur', curNumber)
-                    delCounter[k].push(primDel[k][0]);
-                    curNumber /= primDel[k][0];
-                }
-            }
-            while (curNumber % primDel[k][0] && curNumber > 1)
-
+      delCounter.push([]);
+      for (var j = 0; j <= number; j++) {
+        curNumber = j;
+        do {
+          if (curNumber % primDel[k][0] === 0) {
+            delCounter[delCounter.length-1].push(primDel[k][0]);
+            curNumber /= primDel[k][0];
+          } 
         }
-    }//console.log(delCounter[0].length)
+        while (curNumber % primDel[k][0] === 0 && curNumber >= primDel[k][0])
+          }
+          }
+      console.log(primDel);
+      if(primDel.length > 1){
+        for(var z = 0; z < primDel.length, z < delCounter.length; z++){
+          lengthCounter.push(Math.floor(delCounter[z].length / primDel[z].length));
+          lengthCounter.sort(function(a, b){return a - b;});
+        }
+      } else{
+        return delCounter[0].length;
+      }
+      return lengthCounter[0];
 }
-getZerosCount(16, 8);
